0142f75706cd9de2ede69b228c10f4f70e19b75a
be1z <73556558+be1z@users.noreply.github.com>
2023-01-19 14:10:17 +0100

    Add files via upload

---
+++ palindromo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class palindromo {
+
+    static String tecleaFrase (Scanner e){
+        System.out.print("TECLEA FRASE: ");
+        String fr = e.nextLine();
+        return fr;
+    }
+
+    static String compPalindromo (String frase){
+        int i = 0, i2 = frase.length()-1;
+        boolean sw = true;
+
+        for(i=0;i<frase.length()-1;i++){
+            if(frase.charAt(i) != frase.charAt(i2))
+                sw = false;
+                i = frase.length()-1;
+        }
+        	
+        if(sw == true)
+            System.out.println(frase+" es palíndromo.");
+            else
+                System.out.println(frase+" no es palíndromo.");
+        return frase;
+    }
+
+    public static void main (String [] args){
+        Scanner e = new Scanner(System.in);
+        String frase = "";
+
+        frase = tecleaFrase(e);
+        compPalindromo(frase);
+        e.close();
+    }  
+}
